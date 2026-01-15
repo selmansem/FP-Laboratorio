3607b0bcb29b23f745abf7469473397daa475643
selmansem <sobre_11.1989@hotmail.com>
2026-01-15 19:26:22 +0100

    feat: Función auxiliar para mostrar título de ejercicio en consola

---
+++ src/headers/ejercicios.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void ejercicio(string title) {
+    cout << "Ejercicio: " << title << endl;
+    cout << "------------------------" << endl;
+}
